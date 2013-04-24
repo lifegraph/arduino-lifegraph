ebf5a72290705557160a4ba8ee757def31d006cc
Jia Huang <jialiya.huang@students.olin.edu>
2013-04-24 17:03:29 -0300

    Update lifegraphconnect.ino

--- examples/lifegraphconnect/lifegraphconnect.ino
+++ examples/lifegraphconnect/lifegraphconnect.ino
@@ -22,7 +22,7 @@ const char myPassword[] = "...";
 
 // Lifegraph Connect manages access tokens for us.
 // Create an empty access token we can populate once an RFID card is tagged in.
-char access_token[128] = { 0 };
+char access_token[256] = { 0 };
 
 // We need an application's credentials (namespace, key, and secret)
 // to request a user's access tokens from Facebook.
